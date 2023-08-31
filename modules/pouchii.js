76e7ff76b3eb6c27cf2604a6d59823a5736351be
Adenagbe Emmanuel <emmadenagbe@gmail.com>
2023-08-31 03:59:44 +0100

    used axios

--- modules/pouchii.js
+++ modules/pouchii.js
@@ -1,37 +1,42 @@
+const axios = require('axios');
+
 class PouchiiModule {
-    constructor() {
-      this.baseUrl = "https://stagingapi.pouchii.net/api/";
-      this.bearerToken = process.env.JWT_TOKEN; // Make sure you have the BEARER_TOKEN set in your environment
-    }
-  
-    async validateMeter(meterData) {
-      return this.sendRequest("itex/validate/meter", 'POST', meterData);
-    }
-  
-    async purchaseElectricity(purchaseData) {
-      return this.sendRequest("itex/purchase/electricity", 'POST', purchaseData);
-    }
-  
-    async sendRequest(endpoint, method, requestData) {
-      const apiUrl = this.baseUrl + endpoint;
-      const requestOptions = {
+  constructor() {
+    this.baseUrl = "https://stagingapi.pouchii.net/api/";
+    this.bearerToken = process.env.JWT_TOKEN; // Make sure you have the JWT_TOKEN set in your environment
+  }
+
+  async validateMeter(meterData) {
+    return this.sendRequest("itex/validate/meter", 'POST', meterData);
+  }
+
+  async purchaseElectricity(purchaseData) {
+    return this.sendRequest("itex/purchase/electricity", 'POST', purchaseData);
+  }
+
+  async sendRequest(endpoint, method, requestData) {
+    const apiUrl = this.baseUrl + endpoint;
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${this.bearerToken}`
+    };
+    
+    console.log("API URL:", apiUrl);
+    
+    try {
+      const response = await axios({
         method: method,
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${this.bearerToken}`
-        },
-        body: JSON.stringify(requestData)
-      };
-      console.log("API URL:", requestOptions);
-      try {
-        const response = await fetch(apiUrl, requestOptions);
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error('API request error:', error);
-        throw error;
-      }
+        url: apiUrl,
+        headers: headers,
+        data: requestData
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error('API request error:', error);
+      throw error;
     }
   }
-  
-  module.exports = PouchiiModule;
+}
+
+module.exports = PouchiiModule;
